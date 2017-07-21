9c57d44c93decd3c5294ad69da7294d08e117189
DESKTOP-7JJA5K6\lusiu <luisue@gmail.com>
2017-07-21 16:26:47 +0900

    [BE] 쿼리 수정 완료 inner join과 outter join 수정 및 서브쿼리를 수정함

--- reservation-service/src/main/java/kr/or/reservation/sql/ProductSqls.java
+++ reservation-service/src/main/java/kr/or/reservation/sql/ProductSqls.java
@@ -3,27 +3,28 @@ package kr.or.reservation.sql;
 
 public class ProductSqls {
 
-	public static final String SELECT_BY_CATEGORY = "select product.id ,  product.category_id , product.name,detail.content ,display.place_name, img.file_id " + 
+	public static final String SELECT_BY_CATEGORY =  "select product.id ,  product.category_id , product.name,detail.content ,display.place_name, img.file_id " + 
 			"from  product left outer join product_detail as detail " + 
 			"	on product.id = detail.product_id " + 
 			"left outer join display_info as display " + 
 			"	on product.id = display.product_id " + 
-			"left outer join " + 
-			"( select file.id as file_id ,product_id from file left outer join product_image " + 
-			"	on file.id = product_image.file_id  where product_image.type = 1) as img" + 
-			"    on product.id = img.product_id  " + 
-			"where product.category_id = :category_id " + 
-			"limit :start , 10";
+			"left outer join  product_image as img  on product.id = img.product_id " + 
+			"   inner join file " + 
+			"	on file.id = img.file_id " + 
+			"where product.category_id = :category_id and  img.type = 1 " + 
+			"limit :start, 10";
+	
 	public static final String SELECT_ALL = "select product.id ,  product.category_id , product.name,detail.content ,display.place_name, img.file_id " + 
-			"from  product left outer join product_detail as detail " + 
+			" from product left outer join product_detail as detail " + 
 			"	on product.id = detail.product_id " + 
 			"left outer join display_info as display " + 
 			"	on product.id = display.product_id " + 
-			"left outer join " + 
-			"( select file.id as file_id, product_id from file left outer join product_image " + 
-			"	on file.id = product_image.file_id  where product_image.type = 1) as img" + 
-			"    on product.id = img.product_id  " + 
-			"limit :start , 10";
+			"left outer join product_image as img " + 
+			"	on product.id = img.product_id " + 
+			"inner join file  " + 
+			"	on file.id = img.file_id " + 
+			"where img.type = 1 " + 
+			"limit :start , 10 ";
 	
 	public final static String SELECT_COUNT_ALL = "select count(*) from product";
 	public final static String SELECT_COUNT_BY_CATEGORYID = "select count(*) from product where category_id = :category_id";
