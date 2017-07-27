3768cf7696fc0eee5ee5e4b3c6a3f79d189c5e17
won <smilesj@outlook.kr>
2017-07-27 10:40:16 +0900

    나의예약 테스트 케이스

---
+++ reservation-service/src/test/java/connect/reservation/dao/ReservationDaoTest.java
@@ -0,0 +1,52 @@
+package connect.reservation.dao;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import connect.reservation.config.RootApplicationContextConfig;
+import connect.reservation.domain.ReservationType;
+import connect.reservation.dto.Reservation;
+import connect.reservation.dto.ReservationCount;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = RootApplicationContextConfig.class)
+@Transactional
+public class ReservationDaoTest {
+
+	@Autowired
+	private ReservationDao dao;
+	
+	@Test
+	public void shouldSelect() {
+		int userId = 10;
+		List<Reservation> list = dao.select(userId);
+		assertThat(list.size(), is(4));
+		
+	}
+	
+	@Test
+	public void shouldSelectCount() {
+		int userId = 10;
+		List<ReservationCount> list = dao.selectCount(userId);
+		assertThat(list.size(), is(4));
+		int i = 0;
+		for(ReservationCount count : list) {
+			
+			if(i == 0) {
+				assertThat(count.getReservationType(), is(ReservationType.REQUESTING));
+				assertThat(count.getCnt(), is(1));
+			}
+			i++;
+		}
+	}
+	
+}
