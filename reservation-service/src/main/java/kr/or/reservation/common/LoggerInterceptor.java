64174a4820126a2a7923ad753ccb811ebd00b4f7
DESKTOP-7JJA5K6\lusiu <luisue@gmail.com>
2017-07-27 14:50:49 +0900

    [setting] log 부분 설정중 - 일단 미루고 task 부터 진행할것

--- reservation-service/src/main/java/kr/or/reservation/common/LoggerInterceptor.java
+++ reservation-service/src/main/java/kr/or/reservation/common/LoggerInterceptor.java
@@ -3,33 +3,30 @@ package kr.or.reservation.common;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.log4j.Logger;
-import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-@Repository
-public class LoggerInterceptor extends HandlerInterceptorAdapter {
+public class LoggerInterceptor implements HandlerInterceptor  {
+	Logger log = Logger.getLogger(this.getClass());
+
 	
-    protected Logger log = Logger.getLogger(this.getClass());
-     
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        if (log.isDebugEnabled()) {
-            log.debug("======================================          START         ======================================");
-            log.debug(" Request URI \t:  " + request.getRequestURI());
-            
-        }
-        log.info("들어옴");
-        return super.preHandle(request, response, handler);
-    }
-     
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        if (log.isDebugEnabled()) {
-            log.debug("======================================           END          ======================================\n");
-        }
-    }
-}
-
-
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+		throws Exception {
+		log.debug(" Request URI : " + request.getRequestURI());
+		return true;
+	}
+	
+	@Override
+	public void postHandle(	HttpServletRequest request, HttpServletResponse response,
+			Object handler, ModelAndView modelAndView) throws Exception {
+	}
+	
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+			Object handler, Exception ex) throws Exception {
+	}
+} 
--- reservation-service/src/main/java/kr/or/reservation/controller/MainController.java
+++ reservation-service/src/main/java/kr/or/reservation/controller/MainController.java
@@ -2,6 +2,7 @@ package kr.or.reservation.controller;
 
 import javax.servlet.http.HttpSession;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import kr.or.reservation.api.NaverLogin;
 
 @Controller
 public class MainController {
+	Logger log = Logger.getLogger(this.getClass());
 	
     @GetMapping(path = "/")
     public String viewMain(Model model,HttpSession session){
@@ -24,6 +26,7 @@ public class MainController {
     		String url = login.getLoginURL(session);
         	model.addAttribute("loginURL", url);
     	}
+    	log.info("?");
     	return "mainpage";
     }
  
