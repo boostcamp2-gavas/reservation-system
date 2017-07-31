c7bf29d141eba6f6f6beab47cdaa97ffbd341333
Se-yeong <ilvh2430@gmail.com>
2017-07-31 12:16:03 +0900

    reviewController

---
+++ reservation-service/src/main/java/kr/or/reservation/controller/ReviewController.java
@@ -0,0 +1,29 @@
+package kr.or.reservation.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import kr.or.reservation.service.CommentService;
+
+@Controller
+@RequestMapping("/review")
+public class ReviewController {
+	private CommentService commentService;
+	
+	@Autowired
+	public void setCommentService (CommentService commentService) {
+		this.commentService = commentService;
+	}
+	
+	@GetMapping(path="/{productId}")
+	public ModelAndView getReservation(@PathVariable int reservationId) {
+		ModelAndView model = new ModelAndView("review");
+
+		return model;
+	}
+
+}
