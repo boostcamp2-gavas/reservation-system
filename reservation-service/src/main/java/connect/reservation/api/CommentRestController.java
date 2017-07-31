7552082a325c88e7075e3fb0674a623243c160f8
won <smilesj@outlook.kr>
2017-07-31 17:24:17 +0900

    comment rest api 추가

---
+++ reservation-service/src/main/java/connect/reservation/api/CommentRestController.java
@@ -0,0 +1,26 @@
+package connect.reservation.api;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import connect.reservation.service.CommentService;
+
+@RestController
+@RequestMapping("/api/comments")
+public class CommentRestController {
+	private final CommentService commentService;
+	
+	public CommentRestController(CommentService commentService) {
+		this.commentService = commentService;
+	}
+	
+	@GetMapping("/{productId}")
+	public Map<String, Object> get(@PathVariable int productId, @RequestParam int start){
+		return commentService.getList(productId, (start-1)*10, 10);
+	}
+}
