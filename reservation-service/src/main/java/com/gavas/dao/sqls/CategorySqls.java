9a5723727021181bbd2fa53b07e8d85589d8380f
Seokho Oh <osh8777@gmail.com>
2017-08-12 17:07:22 +0900

    some comment to anntaion, fix convention

--- reservation-service/src/main/java/com/gavas/dao/sqls/CategorySqls.java
+++ reservation-service/src/main/java/com/gavas/dao/sqls/CategorySqls.java
@@ -2,8 +2,8 @@ package com.gavas.dao.sqls;
 
 public class CategorySqls {
     public static final String SELECT_CATEGORY_BY_ID =
-            "SELECT id,name FROM category WHERE id=:id";
+            "SELECT id, name FROM category WHERE id=:id";
 
     public static final String SELECT_CATEGORY_LIST =
-            "SELECT id,name FROM category";
+            "SELECT id, name FROM category";
 }
